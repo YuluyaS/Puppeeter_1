c688989931694bdc62f496ad7133556cfcfb8773
Yuliya.S <yuliyamarketplace@yandex.ru>
2025-03-04 03:11:40 +0400

    The report is generated, the coverage is 100%

--- 7.3/jest/app.js
+++ 7.3/jest/app.js
@@ -2,6 +2,7 @@ function sortByName(inputArray) {
   inputArray.sort(function (a, b) {
     var nameA = a.toLowerCase(),
       nameB = b.toLowerCase();
+
     if (nameA < nameB)
       //сортируем названия по возрастанию
       return -1;
--- 7.3/jest/test/unit/sortByName.test.js
+++ 7.3/jest/test/unit/sortByName.test.js
@@ -2,40 +2,24 @@ const sorting = require("../../app");
 
 describe("Books names test suit", () => {
   it("Books names should be sorted in ascending order", () => {
-    const input = [
+    const input1 = [
       "Гарри Поттер",
       "Властелин Колец",
       "Волшебник изумрудного города",
     ];
 
-    const expected = [
+    const expected1 = [
       "Властелин Колец",
       "Волшебник изумрудного города",
       "Гарри Поттер",
     ];
+    const output1 = sorting.sortByName(input1);
 
-    const output = sorting.sortByName(input);
+    const input2 = ["Гарри Поттер", "Гарри Поттер", "Гарри Поттер"];
+    const expected2 = ["Гарри Поттер", "Гарри Поттер", "Гарри Поттер"];
+    const output2 = sorting.sortByName(input2);
 
-    expect(output).toEqual(expected);
-  });
-
-  it("Books names not should be sorted in ascending order", () => {
-    const input = [
-      "Властелин Колец",
-      
-    ];
-
-    const expected = 
-    [
-      "Властелин Колец",
-      
-    ];
-
-    const output = sorting.sortByName(input);
-
-    
-
-    expect(output).toEqual(expected);
-    //return 0;
+    expect(output1).toEqual(expected1);
+    expect(output2).toEqual(expected2);
   });
 });
