22e2818b600aaa250472e4fe247fa32b84b9ad87
Yuliya.S <yuliyamarketplace@yandex.ru>
2025-03-24 23:54:55 +0400

    find the error

--- 7.4/puppeteer/gh.test.js
+++ 7.4/puppeteer/gh.test.js
@@ -1,12 +1,9 @@
 const puppeteer = require("puppeteer");
 const url = "https://github.com/team";
 
-
-(async () => {
-  const browser = await puppeteer.launch({ headless: false, slowMo: 1000 });
-});
-
-
+async () => {
+  const browser = await puppeteer.launch({ headless: false, slowMo: 500 });
+};
 
 let page;
 
@@ -28,8 +25,11 @@ describe("Github page tests", () => {
     await firstLink.click();
     await page.waitForSelector("h1");
     const title2 = await page.title();
+
+    console.log("title2=");
+
     expect(title2).toEqual("GitHub: Where the world builds software · GitHub");
-  });
+  }, 22000);
 
   test("The first link attribute", async () => {
     const actual = await page.$eval("a", (link) => link.getAttribute("href"));
@@ -40,11 +40,11 @@ describe("Github page tests", () => {
     const btnSelector = ".btn-large-mktg.btn-mktg";
     await page.waitForSelector(btnSelector, {
       visible: true,
-  
     });
     const actual = await page.$eval(btnSelector, (link) => link.textContent);
     expect(actual).toContain("Sign up for free");
-    page.evaluate(() => {debugger})
-
-  });
+    page.evaluate(() => {
+      debugger;
+    });
+  }, 1000);
 });
