d43081541d9cae4e007e0801405c67bc042f1fa5
Yuliya.S <yuliyamarketplace@yandex.ru>
2025-03-27 00:46:17 +0400

    add timeouts

--- 7.4/puppeteer/gh.test.js
+++ 7.4/puppeteer/gh.test.js
@@ -9,7 +9,7 @@ let page;
 
 beforeEach(async () => {
   page = await browser.newPage();
-  await page.goto("https://github.com/team");
+  //await page.goto("https://github.com/team");
 
   //const title = await page.title();
   //console.log(title)
@@ -20,6 +20,10 @@ afterEach(() => {
 });
 
 describe("Github page tests", () => {
+  beforeEach(async () => {
+    await page.goto("https://github.com/team");
+  });
+
   test("The h1 header content'", async () => {
     const firstLink = await page.$("header div div a");
     await firstLink.click();
@@ -28,13 +32,16 @@ describe("Github page tests", () => {
 
     console.log("title2=");
 
-    expect(title2).toEqual("GitHub: Where the world builds software · GitHub");
-  }, 22000);
+    //expect(title2).toEqual("GitHub: Where the world builds software · GitHub");
+    expect(title2).toEqual(
+      "GitHub · Build and ship software on a single, collaborative platform · GitHub"
+    );
+  }, 30000);
 
   test("The first link attribute", async () => {
     const actual = await page.$eval("a", (link) => link.getAttribute("href"));
     expect(actual).toEqual("#start-of-content");
-  });
+  }, 14000);
 
   test("The page contains Sign in button", async () => {
     const btnSelector = ".btn-large-mktg.btn-mktg";
@@ -42,9 +49,34 @@ describe("Github page tests", () => {
       visible: true,
     });
     const actual = await page.$eval(btnSelector, (link) => link.textContent);
-    expect(actual).toContain("Sign up for free");
+    //expect(actual).toContain("Sign up for free");
+    expect(actual).toContain("Get started with Team");
     page.evaluate(() => {
       debugger;
     });
-  }, 1000);
+  }, 19000);
 });
+
+test("The title should equal 'GitHub Features · GitHub' ", async () => {
+  await page.goto("https://github.com/features");
+
+  const actual = await page.title();
+
+  expect(actual).toEqual("GitHub Features · GitHub");
+}, 10000);
+
+test("The title should equal 'GitHub Codespaces · GitHub' ", async () => {
+  await page.goto("https://github.com/features/codespaces");
+
+  const actual = await page.title();
+
+  expect(actual).toEqual("GitHub Codespaces · GitHub");
+}, 10000);
+
+test("The title should equal 'GitHub Actions · GitHub' ", async () => {
+  await page.goto("https://github.com/features/actions");
+
+  const actual = await page.title();
+
+  expect(actual).toEqual("GitHub Actions · GitHub");
+}, 10000);
